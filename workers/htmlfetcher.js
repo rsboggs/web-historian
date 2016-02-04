78d3a2938edf4729097ce32e31f1b01e60f6b3ba
rsboggs <robertstephenboggs@gmail.com>
2016-02-04 11:07:15 -0800

    Implement htmlfetcher

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,39 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var archive = require('../helpers/archive-helpers');
+var _ = require('underscore');
+
+// archive.readListOfUrls(function(urls) {
+//   var urlsToDownload = [];
+//   _.each(urls, function(url) {
+//     archive.isUrlArhived(url, function(isArchived) { 
+//       if(!isArchived) {
+//         urlsToDownload.push(url);
+//       }
+//     });
+//   });
+//   archive.downloadUrls(urlsToDownload);
+// });
+var checkIfArchived = function(urls, callback) {
+  _.each(urls, function(url) {
+    archive.isUrlArchived(url, function(isArchived) { 
+      if(!isArchived) {
+        callback(url);
+      }
+    });
+  });
+};
+
+var findUrlsToDownload = function(callback) {
+  archive.readListOfUrls(function(urls) {
+    var urlsToDownload = [];
+    checkIfArchived(urls, function(url) {
+      urlsToDownload.push(url);
+    });
+   callback(urls);
+  });
+}
+
+findUrlsToDownload(function(urlsToDownload) {
+  archive.downloadUrls(urlsToDownload);
+});
